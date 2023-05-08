78c49d2b310225627e8c9f939de21d8c5e5443fb
devonsydney <devon.boultonmills@gmail.com>
2023-05-08 07:03:53 +0100

    temporarily disable TypeScript error checks due to PricedProduct type mismatch - https://github.com/medusajs/nextjs-starter-medusa/issues/119

--- next.config.js
+++ next.config.js
@@ -7,6 +7,13 @@ module.exports = withStoreConfig({
   images: {
     domains: process.env.NEXT_PUBLIC_IMAGE_DOMAIN.split(","),
   },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 })
 
 console.log("next.config.js", JSON.stringify(module.exports, null, 2))
