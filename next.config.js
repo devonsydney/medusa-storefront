358a42c0232b97180263bfa34efa99d59ef4e9d0
devonsydney <devon.boultonmills@gmail.com>
2023-07-02 14:43:59 -0700

    attempting to get sales channel ID added to guest customers

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@ const { withStoreConfig } = require("./store-config")
 const store = require("./store.config.json")
 
 module.exports = withStoreConfig({
+  output: 'export', // for outputting static site files to dist folder
+  distDir: 'dist', // for outputting static site files to dist folder
   features: store.features,
   reactStrictMode: true,
   images: {
