2e9ef46dd4d03ac14f942fd60dc1c2b97c2dd3ce
Mark Sergienko <ffab00@gmail.com>
2023-10-09 14:50:34 +0100

    Remove unnecessary logs

--- image.ts
+++ image.ts
@@ -9,11 +9,6 @@ export default async function localImagesLoader({
 }) {
   const relativeSrc = new URL(src).pathname
   const encodedSrc = encodeURIComponent(relativeSrc)
-  console.log(process.env.NEXT_PUBLIC_IMAGE_HOST)
-  console.log(
-    process.env.NEXT_PUBLIC_IMAGE_HOST +
-      `/api/resize?url=${encodedSrc}&width=${width}&quality=${quality}`
-  )
   return (
     process.env.NEXT_PUBLIC_IMAGE_HOST +
     `/api/resize?url=${encodedSrc}&width=${width}&quality=${quality}`
