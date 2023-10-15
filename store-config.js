a3c5cc2224154e740d3d055a3dcd16705cf023e6
devonsydney <devon.boultonmills@gmail.com>
2023-10-15 19:41:29 +0100

    disable custom image loader for yarn dev

--- store-config.js
+++ store-config.js
@@ -10,11 +10,14 @@ function withStoreConfig(nextConfig = {}) {
     }
   })
 
-  // disable exporting for yarn dev
+  // disable exporting and custom image loader for yarn dev
   if (process.env.NODE_ENV == 'development') {
     // TODO: may want to do this for Vercel as well in future
     delete nextConfig.output
     delete nextConfig.distDir
+    nextConfig.images = {
+      domains: process.env.NEXT_PUBLIC_IMAGE_DOMAIN.split(","),
+    }
   }
 
   return nextConfig
