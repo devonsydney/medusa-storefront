b88fc19726903be87d4d1063eb8acd4977a90db3
Mark Sergienko <ffab00@gmail.com>
2023-10-17 17:37:36 +0100

    WIP Try to show tanstack dev in prod

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,5 @@
+import * as React from "react"
+
 import { MEDUSA_BACKEND_URL, queryClient } from "@lib/config"
 import { AccountProvider } from "@lib/context/account-context"
 import { CartDropdownProvider } from "@lib/context/cart-dropdown-context"
@@ -9,11 +11,26 @@ import "styles/globals.css"
 import { AppPropsWithLayout } from "types/global"
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 
+const ReactQueryDevtoolsProduction = React.lazy(() =>
+  // @ts-ignore
+  import("@tanstack/react-query-devtools/build/lib/index.prod.js").then(
+    (d) => ({
+      default: d.ReactQueryDevtools,
+    })
+  )
+)
+
 function App({
   Component,
   pageProps,
 }: AppPropsWithLayout<{ dehydratedState?: unknown }>) {
   const getLayout = Component.getLayout ?? ((page) => page)
+  const [showDevtools, setShowDevtools] = React.useState(false)
+
+  React.useEffect(() => {
+    // @ts-ignore
+    window.toggleDevtools = () => setShowDevtools((old) => !old)
+  }, [])
 
   return (
     <MedusaProvider
@@ -34,7 +51,11 @@ function App({
               </StoreProvider>
             </CartProvider>
           </MobileMenuProvider>
-          <ReactQueryDevtools initialIsOpen={true} />
+          {showDevtools && (
+            <React.Suspense fallback={null}>
+              <ReactQueryDevtoolsProduction />
+            </React.Suspense>
+          )}
         </CartDropdownProvider>
       </Hydrate>
     </MedusaProvider>
