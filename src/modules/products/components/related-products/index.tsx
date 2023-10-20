46c4b3364c25e658ca475d63137ff3f7915088e7
devonsydney <devon.boultonmills@gmail.com>
2023-10-20 18:31:17 +0100

    making related products static and pulling only 4 products

--- src/modules/products/components/related-products/index.tsx
+++ src/modules/products/components/related-products/index.tsx
@@ -1,53 +1,24 @@
-import { fetchProductsList } from "@lib/data"
-import usePreviews from "@lib/hooks/use-previews"
-import getNumberOfSkeletons from "@lib/util/get-number-of-skeletons"
-import repeat from "@lib/util/repeat"
-import { Product, StoreGetProductsParams } from "@medusajs/medusa"
-import Button from "@modules/common/components/button"
-import SkeletonProductPreview from "@modules/skeletons/components/skeleton-product-preview"
-import { useCart } from "medusa-react"
-import { useMemo } from "react"
-import { useInfiniteQuery } from "@tanstack/react-query"
+import { useQuery } from "@tanstack/react-query"
 import ProductPreview from "../product-preview"
+import SkeletonProductPreview from "@modules/skeletons/components/skeleton-product-preview"
 import { PricedProduct } from "@medusajs/medusa/dist/types/pricing"
+import { useRegions } from "@lib/hooks/use-layout-data"
+import { fetchRelatedProducts } from "@pages/products/[handle]"
 
 type RelatedProductsProps = {
   product: PricedProduct
 }
 
 const RelatedProducts = ({ product }: RelatedProductsProps) => {
-  const { cart } = useCart()
+  const { data: regions } = useRegions()
+  const region = regions?.[0]
 
-  const queryParams: StoreGetProductsParams = useMemo(() => {
-    const params: StoreGetProductsParams = {}
-
-    if (cart?.id) {
-      params.cart_id = cart.id
-    }
-
-    if (product.collection_id) {
-      params.collection_id = [product.collection_id]
-    }
-
-    if (product.tags) {
-      params.tags = product.tags.map((t) => t.value)
-    }
-
-    params.is_giftcard = false
-
-    return params
-  }, [product, cart?.id])
-
-  const { data, hasNextPage, fetchNextPage, isLoading, isFetchingNextPage } =
-    useInfiniteQuery(
-      [`infinite-products-${product.id}`, queryParams, cart],
-      ({ pageParam }) => fetchProductsList({ pageParam, queryParams }),
-      {
-        getNextPageParam: (lastPage) => lastPage.nextPage,
-      }
-    )
-
-  const previews = usePreviews({ pages: data?.pages, region: cart?.region })
+  const { data } = useQuery({
+    queryFn: () => fetchRelatedProducts(region!),
+    queryKey: [`related-products-${product.handle}`, region],
+    staleTime: Infinity,
+    refetchOnWindowFocus: false,
+  })
 
   return (
     <div className="product-page-constraint">
@@ -61,36 +32,18 @@ const RelatedProducts = ({ product }: RelatedProductsProps) => {
       </div>
 
       <ul className="grid grid-cols-2 small:grid-cols-3 medium:grid-cols-4 gap-x-4 gap-y-8">
-        {previews.map((p) => (
-          <li key={p.id}>
-            <ProductPreview {...p} />
-          </li>
-        ))}
-        {isLoading &&
-          !previews.length &&
-          repeat(8).map((index) => (
-            <li key={index}>
-              <SkeletonProductPreview />
-            </li>
-          ))}
-        {isFetchingNextPage &&
-          repeat(getNumberOfSkeletons(data?.pages)).map((index) => (
-            <li key={index}>
-              <SkeletonProductPreview />
-            </li>
-          ))}
+        {data
+          ? data.map((product) => (
+              <li key={product.id}>
+                <ProductPreview {...product} />
+              </li>
+            ))
+          : Array.from(Array(4).keys()).map((i) => (
+              <li key={i}>
+                <SkeletonProductPreview />
+              </li>
+            ))}
       </ul>
-      {hasNextPage && (
-        <div className="flex items-center justify-center mt-8">
-          <Button
-            isLoading={isLoading}
-            onClick={() => fetchNextPage()}
-            className="w-72"
-          >
-            Load more
-          </Button>
-        </div>
-      )}
     </div>
   )
 }
--- src/pages/products/[handle].tsx
+++ src/pages/products/[handle].tsx
@@ -12,6 +12,9 @@ import { ParsedUrlQuery } from "querystring"
 import { ReactElement } from "react"
 import { NextPageWithLayout, PrefetchedPageProps } from "types/global"
 import { fetchCollectionData, fetchRegionsData, fetchCategoryData } from "@lib/hooks/use-layout-data"
+import { Region } from "@medusajs/medusa"
+import { formatAmount } from "medusa-react"
+import { getPercentageDiff } from "@lib/util/get-precentage-diff"
 
 interface Params extends ParsedUrlQuery {
   handle: string
@@ -21,6 +24,96 @@ const fetchProduct = async (handle: string) => {
   return await medusaClient.products
     .list({ handle })
     .then(({ products }) => products[0])
+}
+
+export const fetchRelatedProducts = async (
+region: Region
+): Promise<ProductPreviewType[]> => {
+  console.log(handle)
+  const products = await medusaClient.products
+    .list({
+      region_id: region.id,
+      is_giftcard: false,
+      limit: 4, // TODO: make sure this doesn't pull the current product
+    })
+    .then(({ products }) => products)
+    .catch((_) => [] as PricedProduct[])
+
+  const productsStructured = products
+    .filter((p) => !!p.variants)
+    .map((p) => {
+      const variants = p.variants as unknown as CalculatedVariant[]
+
+      const cheapestVariant = variants.reduce((acc, curr) => {
+        if (acc.calculated_price > curr.calculated_price) {
+          return curr
+        }
+        return acc
+      }, variants[0])
+
+      const mostExpensiveVariant = variants.reduce((acc, curr) => {
+        if (acc.calculated_price < curr.calculated_price) {
+          return curr
+        }
+        return acc
+      }, variants[0])
+
+      return {
+        id: p.id!,
+        title: p.title!,
+        handle: p.handle!,
+        thumbnail: p.thumbnail!,
+        cheapestPrice: cheapestVariant
+          ? {
+              calculated_price: formatAmount({
+                amount: cheapestVariant.calculated_price,
+                region: region,
+                includeTaxes: false,
+              }),
+              original_price: formatAmount({
+                amount: cheapestVariant.original_price,
+                region: region,
+                includeTaxes: false,
+              }),
+              difference: getPercentageDiff(
+                cheapestVariant.original_price,
+                cheapestVariant.calculated_price
+              ),
+              price_type: cheapestVariant.calculated_price_type,
+            }
+          : {
+              calculated_price: "N/A",
+              original_price: "N/A",
+              difference: "N/A",
+              price_type: "default",
+            },
+        mostExpensivePrice: mostExpensiveVariant
+          ? {
+              calculated_price: formatAmount({
+                amount: mostExpensiveVariant.calculated_price,
+                region: region,
+                includeTaxes: false,
+              }),
+              original_price: formatAmount({
+                amount: mostExpensiveVariant.original_price,
+                region: region,
+                includeTaxes: false,
+              }),
+              difference: getPercentageDiff(
+                mostExpensiveVariant.original_price,
+                mostExpensiveVariant.calculated_price
+              ),
+              price_type: mostExpensiveVariant.calculated_price_type,
+            }
+          : {
+              calculated_price: "N/A",
+              original_price: "N/A",
+              difference: "N/A",
+              price_type: "default",
+            },
+      }
+    })
+  return productsStructured
 }
 
 const ProductPage: NextPageWithLayout<PrefetchedPageProps> = ({ notFound }) => {
@@ -89,9 +182,18 @@ export const getStaticProps: GetStaticProps = async (context) => {
   await queryClient.prefetchQuery(["navigation_collections"], () => fetchCollectionData())
   await queryClient.prefetchQuery(["navigation_categories"], () => fetchCategoryData(2))
 
+  // grab region to use in query for products list
+  const regions = queryClient.getQueryData<any>(["regions"])
+  const region = regions[0]
+
   // prefetch page-specific params
   await queryClient.prefetchQuery([`get_product`, handle], () =>
     fetchProduct(handle)
+  )
+
+  //console.log("from page ",`related-products-${handle}`,region)
+  await queryClient.prefetchQuery([`related-products-${handle}`, region], () =>
+    fetchRelatedProducts(region)
   )
 
   const queryData = await queryClient.getQueryData([`get_product`, handle])
@@ -106,7 +208,7 @@ export const getStaticProps: GetStaticProps = async (context) => {
 
   return {
     props: {
-      dehydratedState: dehydrate(queryClient),
+      dehydratedState: JSON.parse(JSON.stringify(dehydrate(queryClient))),
       notFound: false,
     },
   }
