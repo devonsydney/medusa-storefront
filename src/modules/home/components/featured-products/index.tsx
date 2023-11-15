8bca73c44dbf1672f44cfadcc4be72ec4513171b
devonsydney <devon.boultonmills@gmail.com>
2023-11-15 14:09:16 +0000

    moving latest products text to environment variable

--- src/modules/home/components/featured-products/index.tsx
+++ src/modules/home/components/featured-products/index.tsx
@@ -10,12 +10,16 @@ const FeaturedProducts = () => {
     <div className="py-12">
       <div className="content-container py-12">
         <div className="flex flex-col items-center text-center mb-16">
-          <span className="text-base-regular text-gray-600 mb-6">
-            Latest products
-          </span>
-          <p className="text-2xl-regular text-gray-900 max-w-lg mb-4">
-            Our newest styles are here to help you look your best.
-          </p>
+        {process.env.NEXT_PUBLIC_STORE_LATEST_PRODUCTS_TEXT && (
+          <>
+            <span className="text-base-regular text-gray-600 mb-6">
+              Latest products
+            </span>
+            <p className="text-2xl-regular text-gray-900 max-w-lg mb-4">
+              {process.env.NEXT_PUBLIC_STORE_LATEST_PRODUCTS_TEXT}
+            </p>
+          </>
+        )}
           <UnderlineLink href="/store">Explore products</UnderlineLink>
         </div>
         <ul className="grid grid-cols-2 small:grid-cols-4 medium:grid-cols-5 gap-x-4 gap-y-8">
