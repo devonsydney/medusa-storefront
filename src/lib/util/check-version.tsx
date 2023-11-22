ea681cf69c5a8d5ebb36ee08cc5bcf4798ecdb91
devonsydney <devon.boultonmills@gmail.com>
2023-11-22 13:13:55 +0000

    enabling page refresh on mismatched versions

--- src/lib/util/check-version.tsx
+++ src/lib/util/check-version.tsx
@@ -2,40 +2,34 @@ import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 
 function VersionChecker() {
-  const [version, setVersion] = useState(null);
+  const [pageVersion, setVersion] = useState(null);
   const router = useRouter();
 
   useEffect(() => {
-    // Function to fetch version
+    // Function to check build version from deployed static version.json
+    // Note: In your server settings, please ensure this file is NOT cached in the browser
     const fetchVersion = async () => {
-      console.log("running fetchVersion...")
       try {
         const response = await fetch('/version.json');
         const data = await response.json();
-        console.log (`fetched version: ${data.version}`)
+        console.log (`Build Version: ${data.version}`)
         return data.version;
       } catch (error) {
         console.error('Failed to fetch version:', error);
       }
     };
 
-    // Initial fetch
+    // Initial fetch on page load
     fetchVersion().then(fetchedVersion => {
       setVersion(fetchedVersion);
     });
 
     // Function to check version and reload if different
     const checkVersionAndUpdate = async () => {
-      console.log("running checkVersionAndUpdate...")
       const fetchedVersion = await fetchVersion();
-      console.log(`expected ${fetchedVersion} and page is ${version}`)
-      if (fetchedVersion && fetchedVersion !== version) {
-        console.log("version MISMATCH, updating")
-        console.log("RELOAD PAGE (DISABLED FOR TESTING)")
-        //window.location.reload();
-      }
-      else {
-        console.log("versions MATCH, doing nothing")
+      if (fetchedVersion && fetchedVersion !== pageVersion) {
+        console.log(`Expected Build Version ${fetchedVersion} and got Page Build Version ${pageVersion}, reloading page.`)
+        window.location.reload();
       }
     };
 
@@ -55,7 +49,7 @@ function VersionChecker() {
       window.removeEventListener('focus', checkVersionAndUpdate);
       router.events.off('routeChangeComplete', handleRouteChange);
     };
-  }, [version, router.events]);
+  }, [pageVersion, router.events]);
 
   return ( <></> );
 }
