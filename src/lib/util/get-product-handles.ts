35a0dfa2547798493aed93424af55b9bd75c4501
devonsydney <devon.boultonmills@gmail.com>
2023-12-09 14:37:03 -0500

    removing limit of 25 products in getProductHandles

--- src/lib/util/get-product-handles.ts
+++ src/lib/util/get-product-handles.ts
@@ -2,7 +2,8 @@ import { medusaClient } from "../config"
 
 export const getProductHandles = async (): Promise<string[]> => {
   const products = await medusaClient.products
-    .list({ limit: 25 })
+    .list({ limit: 100 })
+    // TODO: loop query so it will work beyond 100 products
     .then(({ products }) => products)
 
   const handles: string[] = []
