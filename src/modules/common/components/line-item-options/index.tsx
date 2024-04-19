440a32499d922095cbd282e8aad4f9ff7baed2cf
devonsydney <devon.boultonmills@gmail.com>
2024-04-19 22:02:24 +0100

    upgraded typescript, added typing for pluralize added 'any' typing to options in line-item-options

--- src/modules/common/components/line-item-options/index.tsx
+++ src/modules/common/components/line-item-options/index.tsx
@@ -2,12 +2,14 @@ import { ProductVariant } from "@medusajs/medusa"
 
 type LineItemOptionsProps = { variant: ProductVariant }
 
+// TODO: proper typing for options representing the data structure 
+
 const LineItemOptions = ({ variant }: LineItemOptionsProps) => {
   return (
     <div className="text-small-regular text-gray-700">
-      {variant.options.map((option) => {
+      {variant.options.map((option: any) => {
         const optionName =
-          variant.product.options.find((opt) => opt.id === option.option_id)
+          variant.product.options.find((opt: any) => opt.id === option.option_id)
             ?.title || "Option"
         return (
           <div key={option.id}>
